e543b2d157f95230ef15986c4a30a6b370fc74ff
devKartikeya <kartikeya2122008@gmail.com>
2026-01-14 23:01:42 +0530

    Finalize Temp-Converter

---
+++ Temp-Convertor/index.js
@@ -0,0 +1,33 @@
+console.log("Script is running...");
+let display = document.getElementById('display');
+let far = document.getElementById('far');
+let kel = document.getElementById('kel');
+let show = document.getElementById('show');
+
+display.addEventListener("click", () => {
+    show.innerHTML = "";
+})
+
+far.addEventListener("click", () => {
+    const val = display.value;
+    if (val == "") {
+        show.innerHTML = "Please enter a Number !";
+        return;
+    }
+    const numVal = parseInt(val);
+    const result = Math.floor((numVal * 1.8) + 32);
+    show.innerHTML = result + "F";
+    display.value = "";
+})
+
+kel.addEventListener("click", () => {
+    const val = display.value;
+    if (val == "") {
+        show.innerHTML = "Please enter a Number !";
+        return;
+    }
+    let numVal = parseInt(val);
+    const result = Math.floor(numVal + 273.15);
+    show.innerHTML = result + "K";
+    display.value = "";
+})
