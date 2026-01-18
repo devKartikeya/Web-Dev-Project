9e9b70b2eb4cf1a2a100fdadc27615fbab080b2b
devKartikeya <kartikeya2122008@gmail.com>
2026-01-18 14:27:07 +0530

    Add JS and Finalize

--- Percent CalC/index.js
+++ Percent CalC/index.js
@@ -1,1 +1,13 @@
 console.log("Percentage Calculating");
+const result = document.getElementById("result");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const in1 = parseInt(document.getElementById("first").value);
+  const in2 = parseInt(document.getElementById("second").value);
+  const in3 = parseInt(document.getElementById("third").value);
+  const in4 = parseInt(document.getElementById("fourth").value);
+  const in5 = parseInt(document.getElementById("fifth").value);
+  let res = ((in1 + in2 + in3 + in4 + in5) * 100) / 500;
+  result.innerHTML = res;
+});
